0cc9d320bbfeca485196e7e5fb89d0faea9fa1f1
Cheng Qiu <cheng.qiu@booking.com>
2024-01-03 20:22:23 +0800

    2

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -40,7 +40,7 @@ export default function NavLinks() {
             )}
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <p className="hidden md:block">{link.name}123</p>
           </Link>
         );
       })}
